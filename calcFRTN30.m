6df2d3c8ff1afbc8ab8656e3567779fe5596deef
Erik Norlander <eriknorlander@Eriks-MacBook-Pro.local>
2018-04-06 16:58:55 +0200

    uppdated 1.4)

--- calcFRTN30.m
+++ calcFRTN30.m
@@ -10,10 +10,13 @@ avDeg = ones(length(A),1)'*A*ones(length(A),1)/length(A);
 W =[0, 2, 1;
     2, 0, 1;
     1, 1, 0]; 
-augW = W + eye(3);
-[~,D] = eig(augW);
-P = D\augW;
-L = D - augW;
+D = [3, 0, 0;
+     0, 3, 0;
+     0, 0, 2];
+P = D\W;
+L = D - W;
+check1 = P*ones(3,1);
+check2 = L*ones(3,1);
 %% 1.4b)
 balanced_w = W'*ones(3,1);
 avDeg = ones(3,1)'*W*ones(3,1)/3;
